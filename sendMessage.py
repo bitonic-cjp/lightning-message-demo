cb741e6d4d911f5fd63f4ff0d75b0704686c58a8
Corné Plooy <corne@bitonic.nl>
2020-09-09 15:31:47 +0200

    simple sendMessage script

---
+++ sendMessage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from lightningd import lightning
+
+rpc = lightning.LightningRpc('/home/corne/lightning-network/node0/testnet/node0.rpc')
+
+ret = rpc.call('sendmessage',
+	{
+	'destination': '033b83e1f29b0b73633258f27e9c4d97f37cc8a5b84e2a2d8fdd1b5bb43b5db3e0',
+	'msatoshi': 1000,
+	'message': 'Hello',
+	})
+
+print(ret)
+
