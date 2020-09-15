70cc4f7478df1541085ba3bbd1ce106830514311
Corné Plooy <corne@bitonic.nl>
2020-09-15 12:32:23 +0200

    Add some documentation

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+#This is used by receiveMessage.py to lightningd
 receiveNodeRPCFile = '' #Insert the path to the receive node RPC file
+
+#This is used by sendMessage.py to lightningd
 sendNodeRPCFile    = '' #Insert the path to the receive node RPC file
+
+#sendMessage.py sends its message to this node ID.
 receiveNodeID      = '' #Insert the node ID of the receive node
 
